200b43beb1435451e4f36c7a98dbef1efaa158aa
AweysP <aweyspemba@gmail.com>
2022-02-22 12:55:55 -0500

    midday tuesday

---
+++ src/main/java/org/wecancoeit/reviews/Controllers/HashtagController.java
@@ -0,0 +1,30 @@
+package org.wecancoeit.reviews.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.wecancoeit.reviews.Repos.HashtagRepository;
+
+@Controller
+public class HashtagController {
+    private HashtagRepository hashtagRepo;
+
+    public HashtagController(HashtagRepository hashtagRepo) {
+        this.hashtagRepo = hashtagRepo;
+    }
+
+    @RequestMapping("/hashtags")
+    public String showHashtagPage(Model model) {
+        model.addAttribute("hashtags", hashtagRepo.findAll());
+        return "HashtagsTemplate";
+    }
+
+    @RequestMapping("/hashtag/{ID}")
+    public String showHashtagPage(Model model, @PathVariable long ID) {
+        model.addAttribute("hashtag", hashtagRepo.findById(ID));
+        return "HashtagTemplate";
+
+    }
+}
